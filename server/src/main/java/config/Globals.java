3db48037d0fdec0304902565fd2d6dfc6b809e9e
Daniel Lykov <daniil2705@gmail.com>
2022-09-22 17:41:10 +0300

    fix properties and start server

--- server/src/main/java/config/Globals.java
+++ server/src/main/java/config/Globals.java
@@ -1,9 +1,23 @@
 package config;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Globals {
     private static final Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("src/main/resources/config.properties");
+            properties.load(fileInputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static final int SERVER_PORT = Integer.valueOf(properties.getProperty("port"));
     public static final int SENDING_FREQUENCY = Integer.valueOf(properties.getProperty("seconds"));
