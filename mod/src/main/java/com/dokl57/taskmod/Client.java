06bddb98202d6abd66b00d62d1f7f3f95fe80260
Daniel Lykov <daniil2705@gmail.com>
2022-09-23 10:40:42 +0300

    Done with mod and edit server configuration
    Acces to server at new thread in client (mod)

--- mod/src/main/java/com/dokl57/taskmod/Client.java
+++ mod/src/main/java/com/dokl57/taskmod/Client.java
@@ -1,6 +1,9 @@
 package com.dokl57.taskmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,19 +14,27 @@ import java.net.Socket;
 public class Client {
     public static void start()
     {
-
-        try (Socket socket = new Socket("localhost", 4001)) {
-            BufferedReader in
-                    = new BufferedReader(new InputStreamReader(
-                    socket.getInputStream()));
-            while (true) {
-                if(in.readLine()!="") {
-                    LogUtils.getLogger().info("Server replied: " + in.readLine());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try (Socket socket = new Socket("localhost", 4001)) {
+                    BufferedReader in
+                            = new BufferedReader(new InputStreamReader(
+                            socket.getInputStream()));
+                    while (true) {
+                        if(in.readLine()!="") {
+                            LogUtils.getLogger().info("Server replied: " + in.readLine());
+                            if(Minecraft.getInstance().player!=null){
+                                Minecraft.getInstance().player.displayClientMessage(Component.literal(in.readLine()), true);
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        }).start();
 
 
     }
